9a2c2b5f6455902adda76ca62470536fa9614e1c
Anil-KSharma <53290847+Anil-KSharma@users.noreply.github.com>
2019-07-25 12:27:39 +0530

    Add files via upload

---
+++ ExtentionMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public static class ExtentionMethod
+    {
+        public static string ToProper(this String e)
+        {
+            string Proper = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(e);
+            string[] arr = e.Split(' ');
+            string gv = "";
+            foreach (var item in arr)
+            {
+                string s = item.ToLower();
+                string b= s[0].ToString().ToUpper();
+                gv +=" "+ b + s.Substring(1,s.Length-1);
+            }
+            
+
+            return Proper+ "-" + gv.Trim();
+
+        }
+        //static void Main(string[] args)
+        //{
+
+        //    ExtentionMethod e = new ExtentionMethod();
+        //    Console.WriteLine(e.ToProper("aNiL KUMAr"));
+        //    Console.ReadLine();
+        //}
+    }
+}
